fc97688d190977730cfda37a8cd64e6cf27cca66
Pranjal Sapkota <pranjalsapkota77@gmail.com>
2026-07-16 08:01:59 +0545

    Create database_php05.php

---
+++ database_php05.php
@@ -0,0 +1,13 @@
+<?php
+$host='localhost';
+$user='root';
+$pass='pranjal@123';
+
+$conn=mysqli_connect($host,$user,$pass);
+if($conn)
+{
+    echo "Database connected successfully";
+}
+else{
+    echo "Database failed to connect".mysqli_connect_error();
+}
